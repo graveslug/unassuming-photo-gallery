796cdc779daa0b426caa941d6451cb2042735907
Philip Gray <pcgrayiii@icloud.com>
2020-10-30 16:25:15 -0700

    Added a glob pattern to render all view files instead of constantly having to update everything twice

--- views/view.go
+++ views/view.go
@@ -1,13 +1,28 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"path/filepath"
+)
+
+//this is setting up the pattern for the Glob pattern
+var (
+	LayoutDir   string = "views/layouts/"
+	TemplateExt string = ".gohtml"
+)
+
+//function for the glob pattern. "views/layouts/*.gohtml"
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
+}
 
 //NewView parses our view pages to reduce overall maintenance and repeat code in main.go
 func NewView(layout string, files ...string) *View {
-	files = append(files,
-		"views/layouts/footer.gohtml",
-		"views/layouts/navbar.gohtml",
-		"views/layouts/bootstrap.gohtml")
+	files = append(files, layoutFiles()...)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
